8f906c03d3eaf1889fc67050754a1ec041795218
Mohamed Ait Elkadi <mait-elk@e3r3p16.1337.ma>
2024-03-10 20:56:52 +0000

    ..

--- minishell.c
+++ minishell.c
@@ -6,18 +6,13 @@
 /*   By: mait-elk <mait-elk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 20:23:51 by mait-elk          #+#    #+#             */
-/*   Updated: 2024/03/10 20:38:47 by mait-elk         ###   ########.fr       */
+/*   Updated: 2024/03/10 20:56:04 by mait-elk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <minishell.h>
+#include "minishell.h"
 
 int main(int ac, char **av, char **env)
 {
-	while (1)
-	{
-		char *line = readline(">> ");
-		printf("\n[%s]\n", line);
-		free (line);
-	}
+	
 }
