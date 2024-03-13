aad335e60f4dbe3f18fa29f3825f6dfe600d386c
aabouqas <Aissam520@gmail.com>
2024-03-13 02:22:25 +0000

    done

--- minishell.c
+++ minishell.c
@@ -6,11 +6,25 @@
 /*   By: aabouqas <aabouqas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 12:31:13 by aabouqas          #+#    #+#             */
-/*   Updated: 2024/03/13 00:29:34 by aabouqas         ###   ########.fr       */
+/*   Updated: 2024/03/13 02:21:59 by aabouqas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/minishell.h"
+
+char	*get_username(char **env)
+{
+	int		i;
+
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strnstr(env[i], "USER=", ft_strlen(env[i])))
+			return (env[i] + 5);
+		i++;
+	}
+	return ("minishell");
+}
 
 int	execute(char **env)
 {
@@ -19,7 +33,7 @@ int	execute(char **env)
 	char	*line;
 	int		child_pid;
 
-	line = readline("aabouqas@1337$ ");
+	line = readline("%s@1337$ ", get_username(env));
 	cmd = ft_split(line, ' ');
 	if (ft_strncmp(cmd[0], "cd", 2) == 0)
 	{
@@ -41,7 +55,7 @@ int	execute(char **env)
 	return (child_pid);
 }
 
-int main(int ac, char **av, char **env)
+int	main(int ac, char **av, char **env)
 {
 	while (1)
 		waitpid(execute(env), NULL, 0);
