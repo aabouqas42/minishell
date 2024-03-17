cfc4c584962e2dbd1b19505991a28783b0ce4d21
Mohamed Ait Elkadi <mait-elk@e3r3p16.1337.ma>
2024-03-17 15:19:50 +0000

    done

--- utils/env_sort.c
+++ utils/env_sort.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env_sort.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aabouqas <aabouqas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mait-elk <mait-elk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 01:25:36 by aabouqas          #+#    #+#             */
-/*   Updated: 2024/03/17 03:47:06 by aabouqas         ###   ########.fr       */
+/*   Updated: 2024/03/17 15:19:42 by mait-elk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,28 +31,18 @@ int	all_done(t_env *env)
 void	env_sort(t_env *env)
 {
 	t_env	*temp;
-	t_env	*small;
-	t_env	*reset;
 	char	c;
 
-	reset = env;
-	while (all_done(env) == 0)
+	c = 0;
+	while (c != 127)
 	{
 		temp = env;
-		c = 127;
 		while (temp)
 		{
-			if (*temp->name <= c && temp->printed == 0)
-			{
-				small = temp;
-				c = *temp->name;
-			}
+			if (*temp->name == c)
+				printf("%s=%s\n", temp->name, temp->value);
 			temp = temp->next;
 		}
-		if (small && small->printed != 1)
-			printf("%s=%s\n", small->name, small->value);
-		small->printed = 1;
+		c++;
 	}
-	while (reset && (reset = reset->next))
-		reset->printed = 0;
 }
