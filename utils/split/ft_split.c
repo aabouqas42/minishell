8cdb1cb67e029d61d2083247d276c4c5d7358ee2
Mohamed Ait Elkadi <mait-elk@e3r3p16.1337.ma>
2024-03-28 02:07:18 +0000

    done

--- utils/split/ft_split.c
+++ utils/split/ft_split.c
@@ -6,41 +6,11 @@
 /*   By: mait-elk <mait-elk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 12:07:50 by mait-elk          #+#    #+#             */
-/*   Updated: 2024/03/28 02:05:15 by mait-elk         ###   ########.fr       */
+/*   Updated: 2024/03/28 02:07:07 by mait-elk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-
-// char	**realloc_2d(char **origin, char *to_append)
-// {
-// 	size_t	size;
-// 	size_t	i;
-// 	char	**res;
-
-// 	if (origin == NULL)
-// 	{
-// 		res = malloc(sizeof(char *) * 2);
-// 		res[0] = to_append;
-// 		res[1] = NULL;
-// 		return (res);
-// 	}
-// 	size = 0;
-// 	i = 0;
-// 	while (origin && origin[size])
-// 		size++;
-// 	size++;
-// 	res = malloc(sizeof(char *) * (size + 1));
-// 	while (origin[i])
-// 	{
-// 		res[i] = origin[i];
-// 		i++;
-// 	}
-// 	res[i++] =  to_append;
-// 	res[i] = NULL;
-// 	free(origin);
-// 	return (res);
-// }
 
 char	**_split(char *str)
 {
