166959a179bf827c7fc5975ace947a326a01c538
Mohamed Ait Elkadi <mait-elk@e3r5p4.1337.ma>
2024-05-11 20:12:49 +0100

    done

--- signals/handle_signals.c
+++ signals/handle_signals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   handle_signals.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aabouqas <aabouqas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mait-elk <mait-elk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 15:10:00 by mait-elk          #+#    #+#             */
-/*   Updated: 2024/05/10 12:27:30 by aabouqas         ###   ########.fr       */
+/*   Updated: 2024/05/11 20:12:42 by mait-elk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,22 @@ void	sig_handle_sigquit(int sig)
 void	sig_handle_sigint(int sig)
 {
 	t_data	*data;
-	(void)sig;
 
 	data = data_hook(NULL);
-	if (data && data->fix_doubleprt == 2)
+	if (sig == SIGQUIT)
 	{
-		close(0);
-		return ;
+		if (data && data->fix_doubleprt == 2)
+		{
+			close(0);
+			return ;
+		}
+		printf("\n");
+		if (data && data->fix_doubleprt)
+			return ;
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
 	}
-	printf("\n");
-	if (data && data->fix_doubleprt)
-		return ;
-	rl_on_new_line();
-	rl_replace_line("", 0);
-	rl_redisplay();
 }
 
 void	catch_signals(void)
